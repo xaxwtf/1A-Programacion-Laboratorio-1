3d4bcf3284d6aa5caed77cb72d957ba4a9278a0c
alumno <alumno@utnfra.com.ar>
2018-10-01 12:24:33 -0300

    1er commit agregar efecha a comidas

--- abmDinamico/main.c
+++ abmDinamico/main.c
@@ -17,7 +17,16 @@ typedef struct{
     int id;
     char descripcion[20];
 }eSector;
+typedef struct{
+    int id;
+    char descripcion[20];
+}eComida;
 
+typedef struct{
+int idAlmuerzo;
+int idComida;
+int idEmpleado;
+}eAlmuerzo;
 
 void inicializarEmpleados( eEmpleado x[], int tam);
 int buscarLibre( eEmpleado x[], int tam);
@@ -45,8 +54,40 @@ int main()
         {4, "Contable"},
         {5, "Sistemas"}
         };
+    eComida comidas []={
+    {1,"Milanesa"},
+    {2,"Fideos"},
+    {3,"Pizza"},
+    {4,"Sopa"},
+    {5,"Pescado"},
+    };
+    eAlmuerzo almuerzos[]={
+    {100,1111,4},
+    {101,5555,1},
+    {102,4545,3},
+    {103,3232,4},
+    {104,1111,1},
+    {105,5555,5},
+    {106,4545,2},
+    {107,3232,5},
+    {108,1111,2},
+    {109,4545,1},
+    {110,3232,1},
+    {111,1111,4},
+    {112,5555,3},
+    {113,4545,5},
+    {114,3232,2},
+    {115,5555,5},
+    {116,4545,2},
+    {117,3232,3},
+    {118,1111,2},
+    {119,5555,1},
+    {200,4545,3},
+    };
+
 
     inicializarEmpleados(lista, 10);
+    harcodearEmpleados(lista);
 
 
     do
@@ -70,6 +111,14 @@ int main()
             break;
         case 5:
             listar_empleadosxSector(lista,10,sectores,5);
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
             break;
         case 10:
             seguir = 'n';
@@ -136,8 +185,10 @@ int menu()
     printf("3- Modificar\n");
     printf("4- Listar\n");
     printf("5- Listar todos los empleados de un sector\n");
-    printf("6- ordenar por sector y nombre");
-    printf("7- mostrar datatos de el o los que mas ganan de cada sector");
+    printf("6- ordenar por sector y nombre\n");
+    printf("7- mostrar datatos de el o los que mas ganan de cada sector\n");
+    printf("8- listar Almuerzos\n");
+    printf("9- Almuerzos x Empleado\n");
     printf("10- Salir\n");
     printf("Ingrese opcion: ");
     fflush(stdin);
@@ -339,16 +390,53 @@ void listar_empleadosxSector(eEmpleado x[],int tam,eSector sectores[],int tamSec
 void ordenarXsectorYnombre(eEmpleado x[],int tam, eSector sectores[],int tamSector)
 {
     int i,j;
-    for(i=0;i<tamSector,i++)
+    for(i=0;i<tamSector;i++)
     {
         for(j=0;j<tam;j++)
         {
             if(sectores[i].id>x[j].idSector&&x[j].isEmpty==1)
             {
-                if(x[j].nombre)
+                if(x[j].nombre);
             }
         }
     }
 }
+void cargarDescripcionComidas(eComida comidas[], int tamComida, int idComida, char cadena[]){
 
+    int i;
+    for(i=0; i < tamComida; i++){
+        if( comidas[i].id == idComida){
+
+            strcpy(cadena, comidas[i].descripcion);
+            break;
+        }
+    }
+
+
+}
+void harcodearEmpleados(eEmpleado x[])
+{
+    int i;
+    eEmpleado y[]={
+    {1111,"ana",'f',15000,5,1},
+    {3333,"luis",'m',25000,4,1},
+    {4444,"albertod",'m',10000,5,1},
+    {5555,"julia",'f',30000,1,1},
+    {1313,"julieta",'f',23000,2,1},
+    {4545,"andrea",'f',31000,5,1},
+    {3232,"mauro",'m',27000,5,1},
+    };
+    for(i=0;i<7;i++)
+    {
+        y[i]=x[i];
+    }
+}
+void listarAlmuerzos(eAlmuerzo listaAlmuerzo[],int tamAlmuezo,eEmpleado lista[],int tamEmp,eComida comidas[],int tamcomidas)
+{
+    int i,j,k;
+    char comida_descripcion[20];
+    char empleado_nombre[20];
+
+
+}
 
