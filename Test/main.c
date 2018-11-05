57c26b99cd32a5d33a089b4f810b8566e09c0e81
alumno <alumno@utnfra.com.ar>
2018-11-05 12:11:58 -0300

    2do commmit terminar linkenlist

--- Test/main.c
+++ Test/main.c
@@ -6,11 +6,6 @@
 
 int main()
 {
-    int aux;
-    if(get_String_soloNum(&aux,"Indique un numero: ","\n Error,solo se admiten Numeros",3))
-    {
-        printf("\n el numero se cargo correctamente");
-    }
 
     return 0;
 }
